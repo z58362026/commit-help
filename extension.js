61aa1f6e1119f20b7e30a391b64645dbfd75b4f1
wangming <ming.wang24@gientech.com>
2025-07-29 15:52:04 +0800

    feat: 初始化

--- extension.js
+++ extension.js
@@ -3,6 +3,12 @@
 const vscode = require("vscode");
 // 引入各个功能模块
 const { createVisualList } = require("./src/features/visualList");
+const { submitCommit } = require("./src/features/commitSubmit");
+const { copyToClipboard } = require("./src/features/clipboardCopy");
+const { setupAutocomplete } = require("./src/features/terminalAutocomplete");
+const { registerKeyboardShortcut } = require("./src/triggers/keyboardShortcut");
+const { createButtonTrigger } = require("./src/triggers/buttonTrigger");
+const { registerGitCommitTrigger } = require("./src/triggers/gitCommitTrigger");
 
 /**
  * 扩展激活时调用
@@ -15,14 +21,27 @@ function activate(context) {
     const disposable = vscode.commands.registerCommand("commit-helper.helloWorld", () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage("Hello World VS 33333!");
+        vscode.window.showInformationMessage("Hello World VS from commit-helper!");
     });
     // 注册可视化列表命令，传递 context
     const visualListCommand = vscode.commands.registerCommand("commit-helper.showVisualList", () => createVisualList(context));
+    // 注册自动提交命令
+    const submitCommitCommand = vscode.commands.registerCommand("commit-helper.submitCommit", submitCommit);
+    // 注册复制到剪切板命令
+    const copyToClipboardCommand = vscode.commands.registerCommand("commit-helper.copyToClipboard", copyToClipboard);
 
     // 将命令加入订阅，确保扩展卸载时自动清理
     context.subscriptions.push(disposable);
     context.subscriptions.push(visualListCommand);
+    context.subscriptions.push(submitCommitCommand);
+    context.subscriptions.push(copyToClipboardCommand);
+
+    // 注册快捷键触发
+    registerKeyboardShortcut(context);
+    // 注册按钮触发
+    createButtonTrigger(context);
+    // 注册 git commit 触发
+    registerGitCommitTrigger(context);
 }
 
 /**
